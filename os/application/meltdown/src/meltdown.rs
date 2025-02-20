43118e6cd3e2a0badc506262132d49f68ae217ac
Lu Ro <public@lrose.de>
2025-02-20 15:15:11 +0100

    meltdown: read cpu cycle counter using inline assembly

--- os/application/meltdown/src/meltdown.rs
+++ os/application/meltdown/src/meltdown.rs
@@ -6,7 +6,25 @@ extern crate alloc;
 use runtime::*;
 use terminal::{print, println};
 
+use core::arch::asm;
+
+pub fn rdtsc() -> u64 {
+    let high: u32;
+    let low: u32;
+    unsafe {
+        asm!(
+            "rdtsc",
+            out("eax") low,
+            out("edx") high
+        );
+    }
+    ((high as u64) << 32) | (low as u64)
+}
+
 #[unsafe(no_mangle)]
 pub fn main() {
     println!("Meltdown start\n");
+    
+    println!("cpu cycles: {}", rdtsc());
+    println!("cpu cycles: {}", rdtsc());
 }
