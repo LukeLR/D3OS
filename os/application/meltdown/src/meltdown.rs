72795772532116c04f3abca45871b6c7fda67045
Lu Ro <public@lrose.de>
2025-02-20 17:21:47 +0100

    meltdown: First version for detecting cache timings

--- os/application/meltdown/src/meltdown.rs
+++ os/application/meltdown/src/meltdown.rs
@@ -21,7 +21,7 @@ pub fn rdtsc() -> u64 {
     ((high as u64) << 32) | (low as u64)
 }
 
-pub fn maccess(pointer: usize) {
+pub fn maccess(pointer: *const usize) {
     unsafe {
         asm!(
             "mov {tmp}, [{x}]",
@@ -31,10 +31,55 @@ pub fn maccess(pointer: usize) {
     }
 }
 
+pub fn flush(pointer: *const usize) {
+    unsafe {
+        asm!(
+            "clflush [{x}]",
+            x = in(reg) pointer,
+        );
+    }
+}
+
+pub fn detect_flush_reload_threshold() -> u64{
+    let mut reload_time: u64 = 0;
+    let mut flush_reload_time: u64 = 0;
+    let count: u64 = 10000000;
+    let dummy: usize = 0; // Use single value instead of array ok?
+    let pointer: *const usize;
+    let mut start_time: u64;
+    let mut end_time: u64;
+    
+    pointer = &dummy;
+    
+    maccess(pointer);
+    for _ in 0..count {
+        start_time = rdtsc();
+        maccess(pointer);
+        end_time = rdtsc();
+        reload_time += end_time - start_time;
+    }
+    
+    for _ in 0..count {
+        start_time = rdtsc();
+        maccess(pointer);
+        end_time = rdtsc();
+        flush(pointer);
+        flush_reload_time += end_time - start_time;
+    }
+    
+    println!("Total time: reload: {} flush+reload: {}", reload_time, flush_reload_time);
+    reload_time /= count;
+    flush_reload_time /= count;
+    println!("Average time: reload: {} flush+reload: {}", reload_time, flush_reload_time);
+    let threshold = (flush_reload_time + reload_time * 2) / 3;
+    println!("Threshold: {}", threshold);
+    return threshold;
+}
+
 #[unsafe(no_mangle)]
 pub fn main() {
     println!("Meltdown start\n");
     
-    println!("cpu cycles: {}", rdtsc());
-    println!("cpu cycles: {}", rdtsc());
+    println!("Current CPU time: {}", rdtsc());
+    detect_flush_reload_threshold();
 }
