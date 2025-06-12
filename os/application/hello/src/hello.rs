562920d2b9358b7d40b440851f6a77257b74b4ea
Lu Ro <public@lrose.de>
2025-06-12 16:19:27 +0200

    meltdown: Test accessing kernel space from hello app

--- os/application/hello/src/hello.rs
+++ os/application/hello/src/hello.rs
@@ -6,6 +6,7 @@ use concurrent::{process, thread};
 #[allow(unused_imports)]
 use runtime::*;
 use terminal::{print, println};
+use alloc::slice;
 
 #[unsafe(no_mangle)]
 pub fn main() {
@@ -18,5 +19,12 @@ pub fn main() {
     let args = env::args();
     for arg in args {
         println!("  {}", arg);
+    }
+    
+    unsafe {
+        let ptr = 0xb73898 as *const u8;
+        let test_slice = slice::from_raw_parts(ptr, 22);
+        let test_str = str::from_utf8(test_slice).unwrap();
+        println!("Test str: {}, Address: 0x{:x}", test_str, test_str.as_ptr() as u64);
     }
 }
